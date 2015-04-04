ed9a5fb328e55cc17f0ef700aa7438ca0aacaae7
Chris Pena <Cpena@dmshouston.com>
2015-04-04 16:51:50 -0500

    Update server.js

--- server.js
+++ server.js
@@ -34,21 +34,3 @@ exports = module.exports = app;
 
 // Logging initialization
 console.log('MEAN.JS application started on port ' + config.port);
-
-//Azure Specific
-   var http = require('http');
-   var express = require( 'express' );
-    var odata = require( './server/data/odata' );
-    var stringify = require( 'stringify-object' );
-    var config = require("./server/config/config");
-    var bodyParser = require("body-parser");
-    var app = express( );
-   odata.config( app );
-    app.use(bodyParser.json());
-  app.use( express.static( __dirname + "/public" ) );
-   var port = process.env.port || 1337;
-   app.get("/", function(req, res) {
-   res.sendfile("/public/app/views/index.html", { root: __dirname });
-   });
-   http.createServer(app).listen(port);
-   console.log(stringify( process.env ));
